4375776463acacdf468f9d8c6e4ad19691a65c3d
luisass07 <mariasilva.ls@academia.umb.edu.co>
2026-03-03 14:45:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/api/empleados")
+def crear_empleado(nombre: str, cargo: str, salario: float):
+    return {
+        "mensaje": "Empleado registrado correctamente",
+        "nombre": nombre,
+        "cargo": cargo,
+        "salario": salario
+    }
