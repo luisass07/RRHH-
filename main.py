a7dc3b27a720d9e748bdc932fe14670a4b9b30d2
luisass07 <mariasilva.ls@academia.umb.edu.co>
2026-03-03 14:59:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
+from database import Empleado, db
 
 app = FastAPI(title="ERP RRHH API")
 
-# Modelo de datos
-class Empleado(BaseModel):
-    nombre: str
-    cargo: str
-    salario: float
-
-# Simulación base de datos en memoria
-empleados_db: List[Empleado] = []
-
 @app.post("/api/empleados")
 def crear_empleado(empleado: Empleado):
-    empleados_db.append(empleado)
-    return {
-        "mensaje": "Empleado registrado correctamente",
-        "total_empleados": len(empleados_db)
-    }
+    return db.insertar_empleado(empleado)
 
 @app.get("/api/empleados")
 def listar_empleados():
-    return empleados_db
+    return db.listar_empleados()
